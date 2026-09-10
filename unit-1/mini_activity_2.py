477d1a466932747a1f3db3d64036256d55d48c38
thirtyseven <me@graysonv.ca>
2026-09-09 21:54:35 -0400

    ensure the variable names are clear because i need underscores

--- unit-1/mini_activity_2.py
+++ unit-1/mini_activity_2.py
@@ -1,9 +1,9 @@
 #Calculate the perimeter of a rectangle
 
-#get input
-length = int(input("How long is the rectangle? "))
-width = int(input("How wide is the rectangle? "))
+#get dimensions
+rectangle_length = int(input("How long is the rectangle? "))
+rectangle_width = int(input("How wide is the rectangle? "))
 
 #calculate and output
-perimeter = length * 2 + width * 2
-print(f"The perimeter of the rectangle is {perimeter}")
+rectangle_perimeter = rectangle_length * 2 + rectangle_width * 2
+print(f"The perimeter of the rectangle is {rectangle_perimeter}")
