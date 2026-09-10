457067e130c556996ab408c417cd42e00731dcf1
thirtyseven <me@graysonv.ca>
2026-09-09 21:17:47 -0400

    mini activity 2

---
+++ unit-1/mini_activity_2.py
@@ -0,0 +1,9 @@
+#Calculate the perimeter of a rectangle
+
+#get input
+length = int(input("How long is the rectangle? "))
+width = int(input("How wide is the rectangle? "))
+
+#calculate and output
+perimeter = length * 2 + width * 2
+print(f"The perimeter of the rectangle is {perimeter}")
