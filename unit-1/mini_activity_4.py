150148610672aba56ecf59fb27a2beacef4698d3
thirtyseven <me@graysonv.ca>
2026-09-14 14:12:03 -0400

    mini activity four

---
+++ unit-1/mini_activity_4.py
@@ -0,0 +1,18 @@
+"""
+===============================================
+Assignment: Mini Activity 4
+Student Name: Grayson Vantilborg
+Date: 2026-09-14
+
+By typing my name above, I confirm that this is my own work
+and I have not plagiarized or copied code from others or AI sources.
+===============================================
+"""
+
+# =======================================================
+# Question 1: Say your name!
+# Write a program that displays your name!
+# =======================================================
+
+# --- Put your code here ---
+print("My name is Grayson")
