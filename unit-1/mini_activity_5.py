ffdf12d0081fc9be4c646e6911365552dd278e7b
thirtyseven <me@graysonv.ca>
2026-09-14 14:24:01 -0400

    mini activity five

---
+++ unit-1/mini_activity_5.py
@@ -0,0 +1,18 @@
+"""
+===============================================
+Assignment: Mini Activity 5
+Student Name: Grayson Vantilborg
+Date: 2026-09-14
+
+By typing my name above, I confirm that this is my own work
+and I have not plagiarized or copied code from others or AI sources.
+===============================================
+"""
+
+#get the inputs
+num_1 = float(input("Enter your first number: "))
+num_2 = float(input("Enter your second number: "))
+
+#calculate and output
+answer = num_1 + num_2
+print(answer)
